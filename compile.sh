fc5a209fbcde3b350f53b6db7f2cb5c99be61084
Sergio Morales <i.serge23@gmail.com>
2013-03-14 11:38:20 -0600

    Added compressJS to pack all dependencies in a single file

---
+++ compile.sh
@@ -0,0 +1,12 @@
+./compress.sh \
+  lib/jquery-min.js \
+  lib/underscore-min.js \
+  lib/underscore.inflection.js \
+  lib/backbone-min.js \
+  lib/model_binder-min.js \
+  lib/collection_binder-min.js \
+  lib/backbone-relational.js \
+  lib/backbone.paginator.js \
+  lib/handlebars.js \
+  lib/sharkbone.js
+
